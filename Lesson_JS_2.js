2bdbd8f3d8d8e6cdc718c10a098e6ae6e76527f8
HavlovskiyVictor <victor0507776181@ukr.net>
2022-06-23 11:53:38 +0300

    Lesson 2

---
+++ Lesson_JS_2.js
@@ -0,0 +1,51 @@
+// let age = 18
+// let money = 1000
+// let dog = true
+
+// if (age >= 18 && money >= 1000 && dog) {
+//     console.log("Get ticket")
+// } else if (age < 18) {
+//     console.log("Early")
+// } else if (money < 1000) {
+//     console.log("Keep calm and work hard!!!") 
+// } else if (!dog) {
+//     console.log("Get Cat!!!")
+// }
+
+
+
+// let age = 17
+// let money = 900
+// let dog = false
+
+// if (age >= 18 && money >= 1000 && dog) {
+//     console.log("Get ticket")
+// } else {     if (age < 18) {
+//                 console.log("Early")
+// }            if (money < 1000) {
+//                  console.log("Keep calm and work hard!!!") 
+// }            if (!dog) {
+//                 console.log("Get Cat!!!")
+// }}
+
+
+
+let dog_show = true
+let age = 18
+let money = 1000
+let dog = true
+
+// if (age >= 18 && money >= 1000 && dog) {
+//     console.log("Get ticket")
+// } else {     if (age < 18) {
+//                 console.log("Early")
+// }            if (money < 1000) {
+//                  console.log("Keep calm and work hard!!!") 
+// }            if (!dog) {
+//                 console.log("Get Cat!!!")
+// }}
+
+switch (dog_show) {
+    case age >= 18:
+     console.log("Time to Win!!!")
+}
