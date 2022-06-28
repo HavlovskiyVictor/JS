15455ce389dfd3fc6a78b94bfc7388f2ae788d10
HavlovskiyVictor <victor0507776181@ukr.net>
2022-06-28 14:39:20 +0300

    JS_Functions

---
+++ Lesson_JS_3.js
@@ -0,0 +1,147 @@
+// function lesson(){
+//     let text_1 = "-------Good Job"
+//     console.log(text_1)
+// }
+
+// lesson()
+
+
+
+
+// function lesson(){
+//     let text_1 = "-------Good Job"
+//     console.log(text_1)
+// }
+
+// function lesson_1(q1, q2){
+//     let resalt = q1 + q2 
+//     console.log("Summ ==" + resalt)
+// }
+
+// lesson()
+// lesson_1(5, 10)
+
+
+
+// function lesson(){
+//     let text_1 = "-------Good Job"
+//     console.log(text_1)
+// }
+
+// function lesson_1(q1, q2){
+//     // let resalt = q1 + q2 
+//     // console.log("Summ ==" + resalt)
+//     console.log(`Summ == ${q1 + q2}`)
+//     console.log("Summ ==", q1 + q2)
+//     console.log("Summ ==", q1 * q2)
+// }
+
+// lesson()
+// lesson_1(5, 10)
+
+
+
+
+// function lesson(){
+//     let text_1 = "-------Good Job"
+//     console.log(text_1)
+// }
+
+// function lesson_1(q1, q2){
+//     if (q1 > 4){
+//         let m_result = q2*10
+//         console.log(m_result)
+//     }
+// }
+
+// lesson()
+// lesson_1(5, 10)
+
+
+
+// function lesson(){
+//     let text_1 = "-------Good Job"
+//     console.log(text_1)
+// }
+
+// function lesson_1(q1, q2){
+//     if (q1 > 4){
+//         let m_result = q2*10
+//         console.log(m_result)
+//     }
+// }
+
+// function lesson_2(...arguments){
+//     let resalt = 0
+//     for (let arg of arguments){
+//         resalt += arg
+//     }
+//     console.log(resalt)
+// }
+
+// lesson()
+// lesson_1(5, 10)
+// lesson_2(5, 10, 20, 100)
+
+
+
+
+// names = ["Alex", "Inna", "Vadim", "Anatoly", "Natalia"]
+
+// function names_editor(person_name){
+//     let name_1 = "Hello " + person_name + "!"
+//     console.log(name_1)
+//  } 
+
+//  for (let i=0; i<names.length; i++){
+//     names_editor(names[i])
+//  }
+
+
+
+
+// let f1 = () => console.log("Nice")
+// f1()
+
+
+
+
+// let f1 = () => console.log("Nice")
+// f1()
+
+// let t1 = 10;
+// let t2 = 15;
+// let t3 = 20;
+
+// let summ = (n1, n2) => {console.log(`Sum = $(n1 + n2)`)}
+// summ(t1, t2)
+
+
+
+
+// function lesson_pro(){}
+// lesson_pro.prototype.method_1 = function(){
+//     console.log(`method_1`)
+// }
+
+// lesson_pro.prototype.method_2 = function(tt1, tt2){
+//     console.log(`method_2 = ${tt1 + tt2}`)
+// }
+
+// let wood = new lesson_pro()
+// let wood_2 = new lesson_pro()
+
+// wood.method_1()
+// wood.method_2(10, 20)
+
+// wood_2.method_2(100, 500)
+
+
+
+
+// let t1 =20;
+// let f3 = (t1 > 10) ?
+// (tt1, tt2) => console.log(tt1 + tt2):
+// (tt1, tt2) => console.log(tt1 - tt2);
+
+// f3(10, 30)
